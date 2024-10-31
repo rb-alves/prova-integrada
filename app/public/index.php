51ca8ecb766a9b9c18abad130f6a93c1b1f00ba8
Roberto Alves <robertofalves77@gmail.com>
2024-10-31 12:58:04 -0300

    Definição das rotas dos usuários

--- app/public/index.php
+++ app/public/index.php
@@ -18,8 +18,11 @@
 
     // Verifica qual o controller está definida
     switch ($controller) {
+
         // Caso a controller seja questão
         case 'questao':
+
+                // Gerencia as  ações das questões
                 if($action == "list"):
                     // Listar as questoes
                     $questoes = $controllerQuestao->listaQuestoes();
@@ -57,6 +60,8 @@
 
         // Caso o controller seja USUARIO
         case 'usuario':
+
+            // Gerencia as ações dos usuários
             if ($action == 'list'):
                 // Lista os usuários
                 $usuarios = $controllerUsuario->listaUsuarios();
@@ -64,8 +69,37 @@
             elseif ($action == 'create'): // Ação de cadastro
                 // Chama o metodo de cadastro da controller
                 $controllerUsuario->cadastro();
+            elseif ($action == 'edit'): // Ação de Edição
+
+                // Verifica se o id foi definido na url
+                if (isset($_GET['id'])):
+                    // Caso seja a variavel id recebe seu valor
+                    $id = $_GET['id'];
+
+                    // Chama o metodo de edição da controller
+                    $controllerUsuario->edicao($id);
+                else:
+                    // Caso o ID  não seja definido na URL
+                    echo "ID do usuário não foi definido";
+                endif;
+
+            elseif ($action == 'del'): // Ação Delete
+
+                // Verifica se o id foi definido na url
+                if (isset($_GET['id'])):
+                    // Caso seja a variavel id recebe seu valor
+                    $id = $_GET['id'];
+
+                    // Chama o metodo de deletar da controller
+                    $controllerUsuario->apagar($id);
+                else:
+                    // Caso o ID não seja definido na URL
+                    echo "ID do usuário não foi definido";
+                endif;
+
             endif;
             break;
+            
         default:
             # code...
             break;
