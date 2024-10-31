c01c05110488298177305042d1a7b224efea6ddf
Roberto Alves <robertofalves77@gmail.com>
2024-10-31 02:12:46 -0300

    Iniciação views de usuarios.

---
+++ app/views/usuario/criar.php
@@ -0,0 +1,31 @@
+<div>
+    <a href="#">Lista de Usuários</a>
+</div>
+
+<form action="#" method="POST">
+    <div>
+        <label for="nome">Nome</label>
+        <input type="text" id="nome" name="nome" required>
+    </div>
+    <div>
+        <label for="cpf">CPF</label>
+        <input type="text" id="cpf" name="cpf" required>
+    </div>
+    <div>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" required>
+    </div>
+    <div>
+        <label for="telefone">Telefone</label>
+        <input type="text" id="telefone" name="telefone" required>
+    </div>
+    <div>
+        <label for="perfil">Perfil de Acesso</label>
+        <select name="perfil" id="perfil" required>
+            <option value="">Selecione...</option>
+            <?php 
+            
+            ?>
+        </select>
+    </div>
+</form>
